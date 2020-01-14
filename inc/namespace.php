280add0e3ac512627b7ca83cccbda47986171949
Silvan Hagen <neverything@gmail.com>
2020-01-14 16:49:57 +0100

    Added scripts loader helpers in php to register the styles and scripts

--- inc/namespace.php
+++ inc/namespace.php
@@ -25,6 +25,77 @@ function bootstrap() {
  *
  * @since 0.2.0
  */
-function register_scripts() {
-	wp_enqueue_script( 'vuejs' );
+function register() {
+	register_scripts( get_scripts() );
+	register_styles( get_styles() );
+}
+
+/**
+ * Register the individual scripts with WP.
+ *
+ * @since 0.2.0
+ *
+ * @param array $scripts needed to run.
+ */
+function register_scripts( $scripts ) {
+	foreach ( $scripts as $handle => $script ) {
+		$deps      = isset( $script['deps'] ) ? $script['deps'] : false;
+		$in_footer = isset( $script['in_footer'] ) ? $script['in_footer'] : false;
+		$version   = isset( $script['version'] ) ? $script['version'] : VERSION;
+
+		wp_register_script( $handle, $script['src'], $deps, $version, $in_footer );
+	}
+}
+
+/**
+ * Register the stylesheets with WP.
+ *
+ * @since 0.2.0
+ *
+ * @param array $styles needed to style.
+ */
+function register_styles( $styles ) {
+	foreach ( $styles as $handle => $style ) {
+		$deps = isset( $style['deps'] ) ? $style['deps'] : false;
+
+		wp_register_style( $handle, $style['src'], $deps, PLUGIN_VERSION );
+	}
+}
+
+function get_scripts() {
+
+	$scripts = array(
+		'spaces-media-editor-runtime'  => array(
+			'src'       => PLUGIN_ASSETS . '/js/runtime.js',
+			'version'   => filemtime( PLUGIN_PATH . '/assets/js/runtime.js' ),
+			'in_footer' => true,
+		),
+		'spaces-media-editor-vendor'   => array(
+			'src'       => PLUGIN_ASSETS . '/js/vendors.js',
+			'version'   => filemtime( PLUGIN_PATH . '/assets/js/vendors.js' ),
+			'in_footer' => true,
+		),
+		'spaces-media-editor-frontend' => array(
+			'src'       => PLUGIN_ASSETS . '/js/frontend.js',
+			'deps'      => array( 'spaces-media-editor-vendor', 'spaces-media-editor-runtime' ),
+			'version'   => filemtime( PLUGIN_PATH . '/assets/js/frontend.js' ),
+			'in_footer' => true,
+		),
+	);
+
+	return $scripts;
+}
+
+function get_styles() {
+
+	$styles = array(
+		'spaces-media-editor-style'    => array(
+			'src' => PLUGIN_ASSETS . '/css/style.css',
+		),
+		'spaces-media-editor-frontend' => array(
+			'src' => PLUGIN_ASSETS . '/css/frontend.css',
+		),
+	);
+
+	return $styles;
 }
