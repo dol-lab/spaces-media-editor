93b85e86edb4f06f49227ee9acec4a9ea82c6d74
Silvan Hagen <neverything@gmail.com>
2020-01-14 16:48:17 +0100

    Added vue app source

---
+++ src/frontend/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue(
+	{
+		el: '#spaces-media-editor',
+		router,
+		render: h => h( App )
+	}
+)
