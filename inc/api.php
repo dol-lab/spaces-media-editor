e47ebf7b82ade91d77527fc1a413377373080b1f
Silvan Hagen <neverything@gmail.com>
2020-01-14 16:48:56 +0100

    Add api.php for future rest enhancements

---
+++ inc/api.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * REST API class for the spaces media editor.
+ *
+ * @since 0.2.0
+ *
+ * @package Spaces_Media_Editor
+ */
+
+namespace Spaces\Spaces_Media_Editor;
+
+use WP_REST_Controller;
+
+/**
+ * REST_API Handler
+ */
+class API extends WP_REST_Controller {
+
+}
